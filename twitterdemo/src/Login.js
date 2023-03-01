7e9de6ae8402d44818bb9a69f61a02a281536226
Sudeep <16595513+sudeep9feb@users.noreply.github.com>
2023-03-01 22:40:24 +0530

    1st March 2023
    Login validation formik and yup

--- twitterdemo/src/Login.js
+++ twitterdemo/src/Login.js
@@ -1,7 +1,33 @@
 import React from 'react'
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from "yup"
 
 function Login() {
+    // yup validation rules
+    const formSchema = Yup.object({
+        username: Yup.string().required("username is required")
+            .min(5, "no less than 5 characters").max(15, "no more than 15 characters"),
+        email: Yup.string().required("Email is required").email("Email is not valid"),
+        password: Yup.string().required("Password is required")
+    })
+
+    const formik = useFormik({
+        // initial values
+        initialValues: {
+            username: "",
+            email: "",
+            password: ""
+        },
+        // validation rules
+        validationSchema: formSchema,
+        // submission
+        onSubmit: (values) => {
+            console.log(values);
+        }
+    }
+
+    )
     return (
         <Box
             display={"flex"}
@@ -13,27 +39,43 @@ function Login() {
             maxWidth={"300px"}
             padding={5}
         >
-            <Stack direction={"column"} spacing={3}>
-                <TextField
-                    variant='filled'
-                    label="Username"
-                    type={"text"}
-                    placeholder="Please enter the username"
-                />
-                <TextField
-                    variant='filled'
-                    label="Email"
-                    type={"email"}
-                    placeholder="Please enter the email"
-                />
-                <TextField
-                    variant='filled'
-                    label="Password"
-                    type={"password"}
-                    placeholder="Please enter the password"
-                />
-                <Button variant='contained'>Login</Button>
-            </Stack>
+            <form onSubmit={formik.handleSubmit}>
+                <Stack direction={"column"} spacing={3}>
+                    <TextField
+                        variant='filled'
+                        label="Username"
+                        type={"text"}
+                        placeholder="Please enter the username"
+                        name='username'
+                        onChange={formik.handleChange}
+                        error={Boolean(formik.touched.username && formik.errors.username)}
+                        helperText={formik.touched.username && formik.errors.username}
+                    />
+                    <TextField
+                        variant='filled'
+                        label="Email"
+                        type={"email"}
+                        placeholder="Please enter the email"
+                        name='email'
+                        onChange={formik.handleChange}
+                        error={Boolean(formik.touched.email && formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
+                    <TextField
+                        variant='filled'
+                        label="Password"
+                        type={"password"}
+                        placeholder="Please enter the password"
+                        name='password'
+                        onChange={formik.handleChange}
+                        error={Boolean(formik.touched.password && formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+                    <Button type='submit' variant='contained'>
+                        <Typography variant='h5'>Login</Typography>
+                    </Button>
+                </Stack>
+            </form>
         </Box>
 
     )
